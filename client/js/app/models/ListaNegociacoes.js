2f57b59655cafd48580842e7c4dc36fc1c9c5a14
Paulo Ricardo <pauloricardoh@hotmail.com>
2020-04-10 20:20:33 -0300

    API Reflection

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,12 +1,22 @@
 
    class ListaNegociacoes{
 
-      constructor() {
-          this._negociacoes = []
+      constructor(contexto,armadilha) {
+          this._negociacoes = [];
+          this._armadilha = armadilha;
+          this._contexto = contexto;
                     }
 
       adiciona(negociacao){
           this._negociacoes.push(negociacao);
+          //this._armadilha(this);
+          Reflect.apply(this._armadilha,this._contexto,[this]);
+      }
+
+      esvazia(){
+          this._negociacoes =[];
+        //this.armadilha(this);
+        Reflect.apply(this._armadilha,this._contexto,[this]);
       }
 
       get negociacoes()
